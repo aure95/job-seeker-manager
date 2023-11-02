53cacc3683b1f9142ec26296d7ea0162c7c1daa0
aurelien <aurelien.mouret@imie-paris.fr>
2023-11-02 12:15:42 +0100

    added Spring Boot application-api

---
+++ application-api/application-api/src/test/java/com/example/applicationapi/ApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
