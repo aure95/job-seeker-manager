547fa725d7c00af792aa22c6e6b06dc638a8c1d8
aurelien <aurelien.mouret@imie-paris.fr>
2023-11-09 11:53:21 +0100

    added RestResource \application and connect to MongoDB

--- application-api/src/main/java/com/applicationapi/ApplicationApiApplication.java
+++ application-api/src/main/java/com/applicationapi/ApplicationApiApplication.java
@@ -2,6 +2,9 @@ package com.applicationapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+// import com.applicationapi.repositories.ApplicationRepository;
 
 @SpringBootApplication
 public class ApplicationApiApplication {
--- application-api/src/main/java/com/applicationapi/repositories/ApplicationRepository.java
+++ application-api/src/main/java/com/applicationapi/repositories/ApplicationRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 // import org.springframework.data.repository.CrudRepository;
 // import org.springframework.data.mongodb.repository.MongoRepository;
 // import org.springframework.data.repository.query.Param;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "application", path = "application")
-public interface ApplicationRepository extends MongoRepository<Application, Long> {
+@RepositoryRestResource(collectionResourceRel = "applications", path = "applications")
+public interface ApplicationRepository extends MongoRepository<Application, String> {
 
 }
